074d902fe7bbe3d93c5ccf9e1af053b037a9e777
roee dotan <roeedo555@gmail.com>
2023-08-14 09:06:27 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:a1a1a1@localhost/recommended_films'
+db = SQLAlchemy(app)
+
+class Film(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    rate = db.Column(db.Integer, nullable=False)
+
+@app.route('/movies/<int:year>')
+def most_rated_films(year):
+    result = get_most_rated_films(year)
+    return render_template('movies.html', year=year, movies=result)
 
 def get_most_rated_films(year):
     # Connect to the MySQL database
@@ -25,10 +39,8 @@ def get_most_rated_films(year):
 
     return movies
 
-@app.route('/movies/<int:year>')
-def most_rated_films(year):
-    result = get_most_rated_films(year)
-    return render_template('movies.html', year=year, movies=result)
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port="5000")
+    # Create the database schema
+    db.create_all()
+
+    app.run(host='0.0.0.0', port=5000)
